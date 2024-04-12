15624b8d98155b22e6a0b0e3254e3e34a0eceee0
jbrems <jonasbrems@gmail.com>
2024-04-13 00:45:38 +0200

    Use semantically correct logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,10 +1,10 @@
 import style from './Logo.module.css'
 
 export function Logo() {
-  return <div className={style.logo}>
+  return <div className={style.logo} aria-hidden>
     <span>B</span>
-    <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="8" cy="8" r="4" />
+    <svg width="12" height="12" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="9" cy="9" r="8" />
     </svg>
   </div>
 }
